ecc5a081b7af953ec96036e263d440fd93546404
Deepti <sagar.deepti.baghel@gmail.com>
2025-10-30 19:09:56 +0530

    Lambda Functions

---
+++ SmartLightDemo.java
@@ -0,0 +1,17 @@
+
+@FunctionalInterface
+interface LightAction {
+    void activate();
+}
+
+public class SmartLightDemo {
+    public static void main(String[] args) {
+        LightAction motionSensor = () -> System.out.println("Motion detected → Lights ON");
+        LightAction nightMode = () -> System.out.println("Night mode → Dim warm lights ON");
+        LightAction voiceCommand = () -> System.out.println("Voice command → Colorful lights ON");
+
+        motionSensor.activate();
+        nightMode.activate();
+        voiceCommand.activate();
+    }
+}
