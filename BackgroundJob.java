3f13af4caa62e73cb5eda399bb70593c010b2718
Deepti <sagar.deepti.baghel@gmail.com>
2025-10-30 19:01:55 +0530

    Initial commit

---
+++ BackgroundJob.java
@@ -0,0 +1,8 @@
+
+public class BackgroundJob {
+    public static void main(String[] args) {
+        Runnable job = () -> System.out.println("Executing background job...");
+        Thread thread = new Thread(job);
+        thread.start();
+    }
+}
---
+++ CloningPrototype.java
@@ -0,0 +1,17 @@
+
+class Prototype implements Cloneable {
+    int id;
+    Prototype(int id) { this.id = id; }
+
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
+
+public class CloningPrototype {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Prototype obj1 = new Prototype(101);
+        Prototype obj2 = (Prototype) obj1.clone();
+        System.out.println("Cloned object ID: " + obj2.id);
+    }
+}
